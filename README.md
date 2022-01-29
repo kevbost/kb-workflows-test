fad0485beb52b15f9d5e40321666c0c8a46eca96
Kevin Bost <kevbost@gmail.com>
2022-01-29 12:47:24 -0700

    prep for npm publish

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
+# this package is a noop, used only for testing actions, protection rules, et
+
 - 1
 - 2
---
+++ index.js
@@ -0,0 +1,3 @@
+console.log(
+  "this package is a noop, used only for testing actions, protection rules, etc"
+);
